6ef5afd24c9a01493eb6e8092c7d468e67224a87
beanlock <145821604+ygeb1@users.noreply.github.com>
2025-03-31 23:51:17 -0400

    fix welcome images

--- crossscores/app/Welcome.jsx
+++ crossscores/app/Welcome.jsx
@@ -1,21 +1,31 @@
 "use client";
-
 import Image from "next/image"
 import img1 from './assets/img1.png'
 import img2 from './assets/img2.png'
 import img3 from './assets/img3.png'
 import './Welcome.css'
+import { useEffect, useState} from "react";
 
 
 function Welcome() {
-    const images = [img1,img2,img3];
-    let index = Math.floor(Math.random() * 3);
-    let img = images[index]
+    
+    const [img,setImg] = useState(null);
+
+    //let index = Math.floor(Math.random() * 3);
+    //let img = images[index];
 
     function closePopup() {
         document.querySelector(".wcontainer").style.display = "none";
     }
     
+
+    useEffect(() => {
+        const images = [img1,img2,img3];
+        setImg(images[Math.floor(Math.random() * images.length)])
+        
+    },[]);
+
+    if(!img) return null;
 
     return(
         <div className="wcontainer">
