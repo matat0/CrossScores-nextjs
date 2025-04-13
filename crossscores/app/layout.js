2b5525258d88277aaf34fa7ecbe8abb502ad947f
matat0 <mattbangit@gmail.com>
2025-04-13 19:44:07 -0400

    deleted index, fixed css

--- crossscores/app/layout.js
+++ crossscores/app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const nunito = Nunito ({
+  subsets: ['latin'],
+  variable: "--font-nunito",
+})
 
 export const metadata = {
   title: "Cross Scores Tracker",
@@ -19,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={nunito.variable}>
         {children}
       </body>
     </html>
