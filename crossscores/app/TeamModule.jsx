8029db78f97918227cbad7935413af0406197d5c
matat0 <mattbangit@gmail.com>
2025-05-15 05:08:37 -0400

    styling

---
+++ crossscores/app/TeamModule.jsx
@@ -0,0 +1,19 @@
+"use client";
+import './TeamModule/TeamModule.css'
+import {useState} from 'react'
+
+
+
+function TeamModule() {
+
+    return(
+        <>
+            <div className="TeamModule">
+
+            </div>
+        </>
+    );
+
+}
+
+export default TeamModule
