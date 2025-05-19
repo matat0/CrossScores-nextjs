05449eece1ba66ab4b48726a0c2a9d38e58c0097
matat0 <mattbangit@gmail.com>
2025-05-18 22:19:16 -0400

    Update next.config.mjs

--- crossscores/next.config.mjs
+++ crossscores/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+      },
+    ]
+  },
+}
+
 export default nextConfig;
