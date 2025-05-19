1a427b9a1008a594d7df29681390f0405145c2e9
matat0 <mattbangit@gmail.com>
2025-05-18 23:12:12 -0400

    renamed

--- crossscores/app/Welcome.jsx
+++ crossscores/app/Welcome.jsx
@@ -1,12 +1,13 @@
 "use client";
-import Image from "next/image"
-import img1 from './assets/img1.png'
-import img2 from './assets/img2.png'
-import img3 from './assets/img3.png'
-import img4 from './assets/img4.png'
-import img5 from './assets/img5.png'
-import img6 from './assets/img6.png'
-import './Welcome/Welcome.css'
+import Image from "next/image";
+import img1 from './assets/img1.png';
+import img2 from './assets/img2.png';
+import img3 from './assets/img3.png';
+import img4 from './assets/img4.png';
+import img5 from './assets/img5.png';
+import img6 from './assets/img6.png';
+import img7 from './assets/img6.png';
+import './Welcome/Welcome.css';
 import { useEffect, useState} from "react";
 
 
@@ -23,7 +24,7 @@ function Welcome() {
     
 
     useEffect(() => {
-        const images = [img1,img2,img3,img4,img5,img6];
+        const images = [img1,img2,img3,img4,img5,img6, img7];
         setImg(images[Math.floor(Math.random() * images.length)])
         
     },[]);
